de4577cdea5baa3143d88bc30b7e826ee81a7f5b
youngseo Kim <yseokim0212@gmail.com>
2022-04-20 23:09:35 +0900

    first commit

---
+++ somtime_turn.cs
@@ -0,0 +1,21 @@
+﻿public class Sometime_Turn : MonoBehaviour
+{
+    public float angle = 90;  //°¢µµ :Inspector ÁöÁ¤
+    public int maxCount = 50;  //ºóµµ:Inspector ÁöÁ¤ 
+    int count = 0;
+
+    void Start()
+    {   //처음
+        count = 0;
+    }
+
+    void FixedUpdate()
+    {
+        count = count + 1;
+        if (count >= maxCount)
+        {
+            this.transform.Rotate(0, 0, angle);
+            count = 0;
+        }
+    }
+}
