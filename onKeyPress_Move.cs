3ded3b9394cd0bcea45f107b90d7aa60f97602cc
youngseo Kim <yseokim0212@gmail.com>
2022-04-28 20:25:37 +0900

    fifth commit

---
+++ onKeyPress_Move.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnKeyPress_MoveSprite : MonoBehaviour
+{
+    public float speed = 2;
+    float vx = 0;
+    float vy = 0;
+    bool leftFlag = false;
+
+    Rigidbody2D rbody;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        rbody.gravityScale = 0;    //중력 0
+        rbody.constraints = RigidbodyConstraints2D.FreezeRotation;    //충돌해도 회전하지 않도록 
+
+    }
+
+    void Update()
+    {
+        vx = 0;
+        vy = 0;
+
+        if (Input.GetKey("right"))
+        {
+            vx = speed;
+            leftFlag = false;
+        }
+
+        if (Input.GetKey("left"))
+        {
+            vx = -speed;
+            leftFlag = true;
+        }
+
+        if (Input.GetKey("up"))
+        {
+            vy = speed;
+        }
+
+        if (Input.GetKey("down"))
+        {
+            vy = -speed;
+        }
+    }
+
+    int count = 0;
+
+    void FixedUpdate()
+    {
+        if (count == 0)
+        {
+            this.GetComponent<Rigidbody2D>().Velocity = new Vector2(5, 0);
+        }
+        if (count == 50)
+        {
+            this.GetComponent<Rigidbody2D>().Velocity = new Vector2(0, 0);
+        }
+        count = count + 1;
+
+        /*
+       //move
+       this.transform.Translate(vx / 50, vy / 50, 0);
+       //flip img
+       this.GetComponent<SpriteRenderer>().flipX = leftFlag;
+        */
+    }
+}
