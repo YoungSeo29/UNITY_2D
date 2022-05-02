8ed16d29b6600ae0a75267e5877e1e63869d4c40
youngseo Kim <yseokim0212@gmail.com>
2022-05-02 15:14:22 +0900

    8th commit

--- forever_chase.cs
+++ forever_chase.cs
@@ -14,7 +14,7 @@ public class Forever_Chase : MonoBehaviour
     void Start()   //처음에 시행한다
     {
         //목표 오브젝트를 찾아낸다
-        targetObject = GameObject.Fine(targetObjectName);
+        targetObject = GameObject.Find(targetObjectName);
         //중력을 0으로 해서 충돌 시에 회전시키지 않는다
         rbody = GetComponent<Rigidbody2D>();
         rbody.gravityScale = 0;
@@ -30,6 +30,9 @@ public class Forever_Chase : MonoBehaviour
         float vy = dir.y * speed;
         rbody.velocity = new Vector2(vx, vy);
         //이동 방향을 왼쪽에서 오른쪽으로 바꾼다
+        // vx<0 ---> 이동하는 양(=vx)을 보고 왼쪽 오른쪽의 방향을 바꿈.
+        // vx가 0보다 크면 vx<0은 FALSE가 되고, flipX에 FALSE가 들어감. 따라서 "왼쪽 오른쪽 반전한다"는 시행되지 않음.
         this.GetComponent<SpriteRenderer>().flipX = (vx < 0);
+
     }
 }
