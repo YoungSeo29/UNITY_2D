f4a3743af5fcf154d19090a04f1fccf04b0fa7b1
youngseo Kim <yseokim0212@gmail.com>
2022-05-04 01:46:48 +0900

    10th commit

--- onCollision_hide.cs
+++ onCollision_hide.cs
@@ -12,12 +12,14 @@ public class OnCollision_Hide : MonoBehaviour
         //처음엔 아무 것도 하지 않음
     }
 
-    void OnCollisionEnter2D(Collison2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        If(collision.gameObject.name == targetObjectName)          //만약 충돌한 것의 이름이 목표 오브젝트였다면
-        {
-            GameObject hideObject = GameObject.Fine(hideObjectName);
-            hideObject.SetActive(false);                  //삭제함
+
+        if (collision.gameObject.name == targetObjectName)
+        {           //만약 충돌한 것의 이름이 목표 오브젝트였다면
+
+            GameObject hideObject = GameObject.Find(hideObjectName);
+            hideObject.SetActive(false);
         }
     }
 }
