251b7cf674a7be7f3830bf00e16054ad11378b63
youngseo Kim <yseokim0212@gmail.com>
2022-05-12 00:56:25 +0900

    14th commit

---
+++ onMouseDown_createPrefab.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//터치하면 그 곳에 프리팹을 만든다
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject newPrefab;      //만드는 프리팹 :inspector에 지정
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //터치한 위치를 카메라 안에서의 위치로 변환해서
+            var pos = Camera.main.ScreesToWorldPoint(Input.mousePosition + Camera.main.transform.forward);
+            pos.z = -5;            //앞쪽에 표시
+            //새로운 프리팹을만들어서 그 위치로 이동시킨다
+            GameObject newGameObject = Instantiate(newPrefab) as newGameObject;
+            newGameObject.transform.position = pos;
+
+        }
+    }
+}
