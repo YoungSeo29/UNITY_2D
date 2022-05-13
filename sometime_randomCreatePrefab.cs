ace4cb02622aae830f9164bee37053b17de086fd
youngseo Kim <yseokim0212@gmail.com>
2022-05-13 17:44:41 +0900

    15th commit

--- sometime_randomCreatePrefab.cs
+++ sometime_randomCreatePrefab.cs
@@ -18,13 +18,13 @@ public class Sometimes_RandomCreatePrefab : MonoBehaviour
         //이 오브젝트의 범위 내에 랜덤으로
         Vector3 area = GetComponent<SpriteRenderer>().bounds.size;
 
-        Vector3 newPos = this.transtorm.position;
+        Vector3 newPos = this.transform.position;
         newPos.x += Random.Range(-area.x / 2, area.x / 2);
         newPos.y += Random.Range(-area.y / 2, area.y / 2);
         newPos.z = -5;            //앞 쪽에 표시
 
         //프리팹을 만든다
-        GameObject newGameObject = Instantiate(newPrefab) as newGameObject;
+        GameObject newGameObject = Instantiate(newPrefab) as GameObject;
         newGameObject.transform.position = newPos;
     }
 
